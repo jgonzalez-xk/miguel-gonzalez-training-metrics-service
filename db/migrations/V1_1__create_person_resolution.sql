7ce4cdaf56eeee36a119a18c4fc844d600e3d01f
Miguel Gonzalez <miguelgon_8@hotmail.com>
2022-08-04 16:15:24 -0600

    Update migrations

--- db/migrations/V1_1__create_person_resolution.sql
+++ db/migrations/V1_1__create_person_resolution.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS person_resolution (
     id SERIAL NOT NULL PRIMARY KEY,
-    created_at timestamp DEFAULT now(),
-    individual_matches int DEFAULT 0,
-    household_matches int DEFAULT 0,
-    nomatches int DEFAULT 0,
-    errors int DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    individual_matches INT DEFAULT 0,
+    household_matches INT DEFAULT 0,
+    nomatches INT DEFAULT 0,
+    errors INT DEFAULT 0,
     CONSTRAINT person_res_id UNIQUE (id)
 );
--- db/migrations/V1_2__create_batch_loader.sql
+++ db/migrations/V1_2__create_batch_loader.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS batch_loader (
     id SERIAL NOT NULL PRIMARY KEY,
-    created_at timestamp DEFAULT now(),
-    file_name varchar(255) NULL,
-    file_created_at timestamp NULL,
-    file_pickedup_at timestamp NULL,
-    file_processed_at timestamp NULL,
-    num_records int DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    file_name VARCHAR(255) NULL,
+    file_created_at TIMESTAMP NULL,
+    file_pickedup_at TIMESTAMP NULL,
+    file_processed_at TIMESTAMP NULL,
+    num_records INT DEFAULT 0,
     CONSTRAINT batch_loader_id UNIQUE (id)
 );
